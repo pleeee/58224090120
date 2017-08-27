75b292cfaf09a0ea94bc9a45ecd6c93020275b05
pleeee <31383689+pleeee@users.noreply.github.com>
2017-08-27 16:49:12 +0700

    Initial commit

---
+++ 2-38.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("58224090120\n");
+	printf("%-8s %-13s\n","Bawornrat","nalucupchanchai");
+	printf("4/july/1995\n");
+	printf("%119s\n","http://www.npru.ac.th");
+	printf("%65s\n","bye bye");
+	return 0;
+}
